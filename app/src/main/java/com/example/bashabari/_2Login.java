2f79b534ca7bc8bce231a2d1331739dca2cf3ada
MohammadSakil <mahfuzur.shakil@northsouth.edu>
2021-03-21 00:33:02 +0600

    Login page design added

---
+++ app/src/main/java/com/example/bashabari/_2Login.java
@@ -0,0 +1,13 @@
+package com.example.bashabari;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class _2Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__2_login);
+    }
+}
