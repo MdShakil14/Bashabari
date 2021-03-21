f94ef9409044329730135aacb5073970f69f1b34
NasirSunny50 <nasir.uddin08@northsouth.edu>
2021-03-21 16:03:22 +0600

    Registration layout 3 Added

---
+++ app/src/main/java/com/example/bashabari/_3Register.java
@@ -0,0 +1,13 @@
+package com.example.bashabari;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class _3Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__3_register);
+    }
+}
