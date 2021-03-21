4175db30185602091066e855c345ac3b38ef30cb
MohammadSakil <mahfuzur.shakil@northsouth.edu>
2021-03-21 15:52:18 +0600

    MainActivity as first welcome page

--- app/src/main/java/com/example/bashabari/MainActivity.java
+++ app/src/main/java/com/example/bashabari/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.bashabari;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgNextArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgNextArrow = findViewById(R.id.imgNextArrow1);
+        imgNextArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, _1Welcome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
